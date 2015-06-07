ec16f7fdbf0633f118aff9fedd374fbfea19ffa6
Grzesiek19 <grzesyou@wp.pl>
2015-06-07 16:04:09 +0200

    Passed test for the expected value for the 0 term of the Fibonacci sequence

---
+++ FibonacciTDD2/src/com/TDD/example/Fibonacci.java
@@ -0,0 +1,18 @@
+package com.TDD.example;
+
+public class Fibonacci {
+
+	/**
+	 * Gives a value of a Fibonacci sequence for given term.
+	 * 
+	 * @param term, the term for which will be calculated Fibonacci value.
+	 *             
+	 * @return suitable value for given term.
+	 */
+
+	public static int getFiboValue(int term) {
+		if (term == 0)
+			return 0;
+		return 0;
+	}
+}
---
+++ FibonacciTDD2/src/com/TDD/example/FibonacciTest.java
@@ -0,0 +1,15 @@
+package com.TDD.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+	@Test
+	public void testForResultFiboTermFor_0() {
+		Fibonacci fibo = new Fibonacci();
+		int fiboValueFor_0 = fibo.getFiboValue(0);
+		assertEquals(0, fiboValueFor_0);
+	}
+}
