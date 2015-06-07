6149184d963270c5bce30720a1039d4cf529610a
Grzesiek19 <grzesyou@wp.pl>
2015-06-07 16:17:27 +0200

    Passed test for the expected value for the 5th term of the Fibonacci sequence

--- FibonacciTDD2/src/com/TDD/example/Fibonacci.java
+++ FibonacciTDD2/src/com/TDD/example/Fibonacci.java
@@ -5,16 +5,29 @@ public class Fibonacci {
 	/**
 	 * Gives a value of a Fibonacci sequence for given term.
 	 * 
-	 * @param term, the term for which will be calculated Fibonacci value.
-	 *             
+	 * @param term
+	 *            , the term for which will be calculated Fibonacci value.
+	 * 
 	 * @return suitable value for given term.
 	 */
 
 	public static int getFiboValue(int term) {
+		int fibonacci = 1;
 		if (term == 0)
 			return 0;
 		else if (term == 1)
 			return 1;
-		return 0;
+		else {
+			int fibo1 = 1;
+			int fibo2 = 1;
+
+			for (int i = 3; i <= term; i++) {
+
+				fibonacci = fibo1 + fibo2;
+				fibo1 = fibo2;
+				fibo2 = fibonacci;
+			}
+			return fibonacci;
+		}
 	}
 }
